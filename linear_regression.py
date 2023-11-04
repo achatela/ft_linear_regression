32b13115feeb11162c772bae5926f5b49884ff49
Alexis CHATELAIN <achatela@made-f0Br4s6.clusters.42paris.fr>
2023-11-04 15:45:09 +0100

    added plotting data

--- linear_regression.py
+++ linear_regression.py
@@ -1,15 +1,16 @@
 import csv
+import matplotlib.pyplot as plt
 
 def estimatePrice(mileage, theta0, theta1):
     return float(theta0) + (float(theta1) * float(mileage))
 
 def parse_csv():
-   dataList = []
-   with open('data.csv', newline='') as csvfile:
-       reader = csv.DictReader(csvfile)
-       for row in reader:
-           dataList.append(dict(row))
-   return dataList
+    dataList = []
+    with open('data.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            dataList.append(dict(row))
+    return dataList
 
 def main():
     dataDict = []
@@ -22,6 +23,8 @@ def main():
 
     L = 0.0000000001 # Learning rate
     epochs = 100
+    prev_cost = float('inf')
+    tol = 1e-6
 
     for r in range(epochs):
         c_tmp = sum(estimatePrice(item['km'], c, m) - float(item['price']) for item in dataDict)
@@ -30,7 +33,24 @@ def main():
         m = m - L * (1/len(dataDict)) * m_tmp
         # print(m, c)
 
+        current_cost = sum((estimatePrice(item['km'], c, m) - float(item['price']))**2 for item in dataDict)
+
+        if abs(current_cost - prev_cost) < tol:
+            break
+
+        prev_cost = current_cost
+
     print(estimatePrice(24000, c, m))
+
+    line = [str(int(estimatePrice(24000, c, m))), str(int(estimatePrice(240000, c, m)))]
+    priceLine = [str(24000), str(240000)]
+    dataDict.sort(key=lambda x: x['price'], reverse=True)
+    # plt.figure(1)
+    # plt.plot([data['km'] for data in dataDict], [data['price'] for data in dataDict], 'o')
+    # plt.plot(priceLine, line, '-m')
+    # plt.grid(True)
+    # plt.show()
+    # plt.close()
 
 main()
 
